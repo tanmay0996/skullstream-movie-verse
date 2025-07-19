a905b7b1faf666275e16f3cb072ece67ad6acdc7
tanmay0996 <167513949+tanmay0996@users.noreply.github.com>
2025-07-20 00:43:05 +0530

    api client

---
+++ src/lib/tmdb.ts
@@ -0,0 +1,15 @@
+// src/lib/tmdb.ts
+
+//This is an api client 
+import axios from 'axios';
+
+const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
+
+export const tmdb = axios.create({
+  baseURL: TMDB_BASE_URL,
+  params: {
+    api_key: TMDB_API_KEY,
+    language: 'en-US',
+  },
+});
